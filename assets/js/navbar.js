5d0244d984dcdd818b959b866d8c2ddda37c4488
Mengi <56649349+MengiFFW@users.noreply.github.com>
2020-11-27 13:39:29 +0100

    changes navbar depending on colormode

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,8 +2,11 @@ function toggleDM() {
 
   if (document.getElementById("toggleDarkmode").checked == true) {
     document.getElementById("cssDarkmode").disabled=false;
+    document.getElementById("navbar").className="navbar navbar-expand-lg navbar-dark bg-dark";
+
   }
   else {
     document.getElementById("cssDarkmode").disabled=true;
+    document.getElementById("navbar").className="navbar navbar-expand-lg navbar-light bg-light";
   }
 }
